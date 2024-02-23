1b4caf546b800b4f535cb8d67dfcf9e8b416b6d9
Daniyal Ganyatov <deni12349075@gmail.com>
2024-02-24 01:22:38 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using static System.Console;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        WriteLine();
+        WriteLine("List of commands");
+        WriteLine("1 – command have array with: [“Hello”, “2”, “world”, “:-)”]");
+        WriteLine("2 – command have array with: [“1234”, “1567”, “-2”, “computer science”]");
+        WriteLine("3 – command have array with: [“Russia”, “Denmark”, “Kazan”]");
+        WriteLine();
+
+
+        int com;
+        string[] array = new string[] { };
+
+        Write("Enter command: ");
+        com = Convert.ToInt16(ReadLine());
+
+    }
+}
