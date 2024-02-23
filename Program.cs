c55a64e0f2ee24d99f32861abbff7574ab7cf3e2
Daniyal Ganyatov <deni12349075@gmail.com>
2024-02-24 01:57:06 +0200

    add switch case

--- Program.cs
+++ Program.cs
@@ -19,5 +19,21 @@ internal class Program
 
         Write("Enter command: ");
         com = Convert.ToInt16(ReadLine());
+
+        switch (com)
+        {
+            case 1:
+                array = new string[] { "Hello", " ", "2", " ", "world", " ", ":-)" };
+                break;
+            case 2:
+                array = new string[] { "1234", " ", "1567", " ", "-2", " ", "computer science" };
+                break;
+            case 3:
+                array = new string[] { "Russia", " ", "Denmark", " ", "Kazan" };
+                break;
+            default:
+                WriteLine($"{com} - That command don't have sorry try again");
+                break;
+        }
     }
 }
